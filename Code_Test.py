11662112d294e256cf4dec1cb9ae27703e7c396f
Thaweesak Jantasan <em.94xd@gmail.com>
2026-06-13 03:01:14 +0700

    TEST date input

--- Code_Test.py
+++ Code_Test.py
@@ -1,16 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.edge.options import Options
+from datetime import date, timedelta
+import pyautogui
 
-import time
 
-options = Options()
-options.add_argument(r"C:\Users\dutymanager.vkhl\AppData\Local\Microsoft\Edge\User Data\Default")
-options.add_argument("profile-directory=Default") # Use the default profile
-options.add_argument("--start-maximized") # Start the browser maximized
-options.add_experimental_option("detach", True) # Keep the browser open after the script finishes
+def yesterday():
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    return yesterday.strftime("%d.%m")
 
-# Launch with the specified options
-driver = webdriver.Edge(options=options)
-driver.get("https://mtca2.oraclehospitality.ap-singapore-1.ocs.oraclecloud.com/MINOR/operacloud/faces/opera-cloud-index/OperaCloud")
-
-# time.sleep(86400)  # Keep the browser open for 24 hours (86400 seconds)
+# print(f"IS {yesterday()}")
+pyautogui.write(f"IS: {yesterday()}")
