a0b1c691d7a64391794eb055bcf4116eb88447ea
Thaweesak Jantasan <em.94xd@gmail.com>
2026-06-16 03:38:41 +0700

    ADD VKHL Arrivals ADD Code_Store file

--- Report/End-day_after/Main_End-day_after.py
+++ Report/End-day_after/Main_End-day_after.py
@@ -1,5 +1,7 @@
 import pyautogui
 import time
+from datetime import date
+from Code_Store import *
 
 # Open Opera
 pyautogui.press("win")
@@ -24,4 +26,65 @@ pyautogui.press("right", presses=6, interval=0.01)
 pyautogui.press("down", interval=0.01)
 pyautogui.press("enter", interval=0.01)
 time.sleep(4.5)
+pyautogui.press("tab", interval=0.01)
+
+# 4. VKHL Arrivals
+pyautogui.write("Arrivals: Detailed FO", interval=.01)
+pyautogui.press("enter", interval=.01)
+time.sleep(2.5)
+pyautogui.press("tab", presses=9, interval=.01)
+pyautogui.press("down", presses=2, interval=.01)
+time.sleep(1.5)
+pyautogui.press("right", interval=.01)
+pyautogui.press("tab", presses=3, interval=.01)
+pyautogui.press("enter", interval=.01)
+time.sleep(3.5)
+# 4. VKHL Arrivals: config
+pyautogui.hotkey("ctrl", "a", interval=.01)
+
+def format1_today():
+    today = date.today()
+    return today.strftime("%d%m")
+
+pyautogui.write(format1_today(), interval=.01)
+pyautogui.press("tab", presses=2, interval=.01)
+time.sleep(1)
+pyautogui.write(format1_today(), interval=.01)
+pyautogui.press("tab", presses=4, interval=.01)
+time.sleep(.75)
+
+Room_Type = "1H2XK,1H2XT,1H3XK,1H4XK,1H4XT,2U1XKT,2U2XKT,2U3XKT,1V1XK,1V2XK,3U1CKT,2V1C2K,1H1VK,1U1VK,1U2VK,2U1VKT,2U2VKT,2U3VKT,3U2VKT,1H2VK,2U4XKT,3U1C2K"
+
+pyautogui.write(Room_Type, interval=.01)
+pyautogui.press("tab", presses=38, interval=.01)
+pyautogui.press("delete", interval=.01)
+time.sleep(.5)
+pyautogui.press("tab", presses=11, interval=.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(8.5)
+# 4. VKHL Arrivals: Save
+pyautogui.click(600, 84, interval=0.01)
+time.sleep(.5)
+pyautogui.hotkey("ctrl", "s", interval=.01)
+time.sleep(1.5)
+pyautogui.hotkey("ctrl", "f", interval=.01)
+tab_reserve(2)
+time.sleep(0.1)
+pyautogui.press("enter", interval=0.01)
+pyautogui.write(Endday_after_folder)
+pyautogui.press("enter", interval=0.01)
+time.sleep(.5)
+pyautogui.press("tab", presses=6, interval=.01)
+
+VKHL_Arrivals = "4. VKHL Arrivals"
+
+pyautogui.write(VKHL_Arrivals)
+pyautogui.press("tab", presses=3, interval=.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(0.5)
+pyautogui.hotkey("ctrl", "w")
+time.sleep(0.5)
+tab_reserve(3)
+pyautogui.press("enter", interval=0.01)
+time.sleep(5)
 pyautogui.press("tab", interval=0.01)
