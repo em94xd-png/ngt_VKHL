ba818c05c30df740ae49362ba03ac031659a64b7
Thaweesak Jantasan <em.94xd@gmail.com>
2026-06-16 02:40:56 +0700

    START End-day_after

---
+++ Report/End-day_after/Main_End-day_after.py
@@ -0,0 +1,27 @@
+import pyautogui
+import time
+
+# Open Opera
+pyautogui.press("win")
+
+site_OPERA = "https://mtca2.oraclehospitality.ap-singapore-1.ocs.oraclecloud.com/MINOR/operacloud/faces/opera-cloud-index/OperaCloud"
+
+pyautogui.write(site_OPERA)
+time.sleep(0.5)
+pyautogui.press("enter")
+
+# In Opera  
+time.sleep(1)
+pyautogui.hotkey("win", "up", "down", "up" ,interval=.1)
+time.sleep(0.1)
+pyautogui.hotkey("ctrl", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-",)
+pyautogui.hotkey("ctrl", "=", "=", "=",)
+time.sleep(5)
+
+# To report search
+pyautogui.press("tab", presses=5, interval=0.01)
+pyautogui.press("right", presses=6, interval=0.01)
+pyautogui.press("down", interval=0.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(4.5)
+pyautogui.press("tab", interval=0.01)
