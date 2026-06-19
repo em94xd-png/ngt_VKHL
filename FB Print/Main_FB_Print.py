782d2924d4f537c449a6c689165a8daccca43da2
Thaweesak Jantasan <em.94xd@gmail.com>
2026-06-20 05:41:08 +0700

    ADD FB Print

---
+++ FB Print/Main_FB_Print.py
@@ -0,0 +1,246 @@
+import subprocess
+import pyautogui
+import time
+from datetime import date, timedelta
+
+site_OPERA = "https://mtca2.oraclehospitality.ap-singapore-1.ocs.oraclecloud.com/MINOR/operacloud/faces/opera-cloud-index/OperaCloud"
+
+Room_Type = "1H2XK,1H2XT,1H3XK,1H4XK,1H4XT,2U1XKT,2U2XKT,2U3XKT,1V1XK,1V2XK,3U1CKT,2V1C2K,1H1VK,1U1VK,1U2VK,2U1VKT,2U2VKT,2U3VKT,3U2VKT,1H2VK,2U4XKT,3U1C2K"
+
+def format1_today():
+    today = date.today()
+    return today.strftime("%d%m")
+
+def page_print(times):
+    pyautogui.hotkey("ctrl", "p", interval=.01)
+    time.sleep(1)
+    pyautogui.press("tab", presses=6, interval=.01)          
+    pyautogui.press("up", presses=2, interval=.01)
+    pyautogui.press("down", presses=(times), interval=.01)
+    pyautogui.press("tab", interval=.01)
+    pyautogui.press("enter", interval=.01)
+    time.sleep(.01)
+    pyautogui.press("tab", presses=3, interval=.01)
+    pyautogui.press("up", presses=2, interval=.01)
+    pyautogui.press("down", presses=(times), interval=.01)
+    pyautogui.press("tab", presses=3, interval=.01)
+    # pyautogui.press("enter", interval=.01)
+    time.sleep(5)
+    pyautogui.hotkey("ctrl", "w", interval=.01)
+
+def page_print_copy2(times):
+    pyautogui.hotkey("ctrl", "p", interval=.01)
+    time.sleep(1)
+    pyautogui.press("tab", presses=3, interval=.01)
+    pyautogui.write("2", interval=.01)
+    pyautogui.press("tab", presses=4, interval=.01)
+    pyautogui.press("up", presses=2, interval=.01)
+    pyautogui.press("down", presses=(times), interval=.01)
+    pyautogui.press("tab", interval=.01)
+    pyautogui.press("enter", interval=.01)
+    time.sleep(.01)
+    pyautogui.press("tab", presses=3, interval=.01)
+    pyautogui.press("up", presses=2, interval=.01)
+    pyautogui.press("down", presses=(times), interval=.01)
+    pyautogui.press("tab", presses=3, interval=.01)
+    # pyautogui.press("enter", interval=.01)
+    time.sleep(5)
+    pyautogui.hotkey("ctrl", "w", interval=.01)
+
+def tab_reserve(times):
+    for _ in range(times):
+        pyautogui.hotkey("shift", "tab")
+
+def format1_today():
+    today = date.today()
+    return today.strftime("%d%m")
+
+# Open Opera
+subprocess.run(["cmd", "/c", "start", "msedge", site_OPERA])
+
+# In Opera  
+time.sleep(1)
+pyautogui.hotkey("win", "up", "down", "up" ,interval=.1)
+time.sleep(0.1)
+pyautogui.hotkey("ctrl", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-",)
+pyautogui.hotkey("ctrl", "=", "=", "=",)
+time.sleep(5)
+
+# To report search
+pyautogui.press("tab", presses=5, interval=0.01)
+pyautogui.press("right", presses=6, interval=0.01)
+pyautogui.press("down", interval=0.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(4.5)
+pyautogui.press("tab", interval=0.01)
+
+# VKHL Arrivals
+pyautogui.write("Arrivals: Detailed FO", interval=.01)
+pyautogui.press("enter", interval=.01)
+time.sleep(3)
+pyautogui.press("tab", presses=9, interval=.01)
+pyautogui.press("down", presses=2, interval=.01)
+time.sleep(1.5)
+pyautogui.press("right", interval=.01)
+pyautogui.press("tab", presses=3, interval=.01)
+pyautogui.press("enter", interval=.01)
+time.sleep(3.5)
+# VKHL Arrivals: Config
+pyautogui.hotkey("ctrl", "a", interval=.01)
+pyautogui.write(format1_today(), interval=.01)
+pyautogui.press("tab", presses=2, interval=.01)
+time.sleep(1)
+pyautogui.write(format1_today(), interval=.01)
+pyautogui.press("tab", presses=4, interval=.01)
+time.sleep(.75)
+pyautogui.write(Room_Type, interval=.01)
+pyautogui.press("tab", presses=38, interval=.01)
+pyautogui.press("delete", interval=.01)
+time.sleep(.5)
+pyautogui.press("tab", presses=11, interval=.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(10)
+# VKHL Arrivals: Print
+pyautogui.click(600, 84, interval=0.01)
+time.sleep(.5)
+page_print(1)
+tab_reserve(3)
+pyautogui.press("enter", interval=0.01)
+time.sleep(5)
+pyautogui.press("tab", interval=0.01)
+
+# VKHL Departures
+pyautogui.write("departure_all", interval=.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(3)
+pyautogui.press("tab", presses=9, interval=0.01)
+pyautogui.press("down", presses=2, interval=0.01)
+time.sleep(1.5)
+pyautogui.press("right", interval=.01)
+pyautogui.press("tab", presses=3, interval=0.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(3.5)
+# VKHL Departures: Config
+pyautogui.press("tab", presses=3, interval=0.01)
+time.sleep(.75)
+pyautogui.write(Room_Type, interval=.01)
+pyautogui.press("tab", presses=10, interval=.01)
+pyautogui.press("space", interval=.01) # Pseudo Rooms
+pyautogui.press("tab", presses=12, interval=.01)
+pyautogui.press("space", interval=.01) # Membership Type
+time.sleep(.5)
+pyautogui.press("tab", interval=.01)
+pyautogui.press("space", interval=.01) # Membership Level
+time.sleep(.5)
+pyautogui.press("tab", presses=13, interval=.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(8.5)
+# VKHL Departures: Print
+pyautogui.click(600, 84, interval=0.01)
+time.sleep(.5)
+page_print(1)
+tab_reserve(3)
+pyautogui.press("enter", interval=0.01)
+time.sleep(5)
+pyautogui.press("tab", interval=0.01)
+
+# VKHL Guests INH
+pyautogui.write("gibyroom", interval=.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(3)
+pyautogui.press("tab", presses=9, interval=0.01)
+pyautogui.press("down", presses=2, interval=0.01)
+time.sleep(1.5)
+pyautogui.press("right", interval=.01)
+pyautogui.press("tab", presses=3, interval=0.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(3.5)
+# VKHL Guests INH: Config
+pyautogui.press("tab", interval=.01)
+time.sleep(.75)
+pyautogui.write(Room_Type, interval=.01)
+pyautogui.press("tab", presses=5, interval=.01)
+pyautogui.press("space", interval=.01) # Pseudo Rooms
+pyautogui.press("tab", presses=5, interval=.01)
+pyautogui.press("space", interval=.01) # Accompanying Names
+pyautogui.press("tab", presses=3, interval=.01)
+pyautogui.press("space", interval=.01) # Notes
+time.sleep(1)
+pyautogui.press("tab", presses=2, interval=.01)
+pyautogui.write("Resv. - GEN", interval=.01)
+time.sleep(.5)
+pyautogui.press("tab", presses=7, interval=.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(12.5)
+# VKHL Guests INH: Print
+pyautogui.click(600, 84, interval=0.01)
+time.sleep(.5)
+page_print(1)
+tab_reserve(3)
+pyautogui.press("enter", interval=0.01)
+time.sleep(5)
+pyautogui.press("tab", interval=0.01)
+
+# Package Forecast
+pyautogui.write("Package Forecast - Detailed", interval=.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(3)
+pyautogui.press("tab", presses=9, interval=0.01)
+pyautogui.press("down", presses=2, interval=0.01)
+time.sleep(1.5)
+pyautogui.press("right", interval=.01)
+pyautogui.press("tab", presses=3, interval=0.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(3.5)
+# Package Forecast: Config
+pyautogui.press("tab", presses=2, interval=0.01)
+pyautogui.write(format1_today(), interval=.01)
+pyautogui.press("tab", interval=0.01)
+time.sleep(1)
+pyautogui.write(format1_today(), interval=.01)
+pyautogui.press("tab", presses=3, interval=0.01)
+time.sleep(.75)
+pyautogui.write("BFB01E,BFB01I,BFB02E,BFB02I,BFB03I,BFB04I", interval=.01)
+pyautogui.press("tab", presses=13, interval=0.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(8.5)
+# Package Forecast: Print
+pyautogui.click(600, 84, interval=0.01)
+time.sleep(.5)
+page_print(1)
+tab_reserve(3)
+pyautogui.press("enter", interval=0.01)
+time.sleep(5)
+pyautogui.press("tab", interval=0.01)
+
+# VKHL Guests INH (Full)
+pyautogui.write("gibyroom", interval=.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(3)
+pyautogui.press("tab", presses=9, interval=0.01)
+pyautogui.press("down", presses=2, interval=0.01)
+time.sleep(1.5)
+pyautogui.press("right", interval=.01)
+pyautogui.press("tab", presses=3, interval=0.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(3.5)
+# VKHL Guests INH (Full): Config
+pyautogui.press("tab", interval=.01)
+time.sleep(.75)
+pyautogui.write(Room_Type, interval=.01)
+pyautogui.press("tab", presses=5, interval=.01)
+pyautogui.press("space", interval=.01) # Pseudo Rooms
+pyautogui.press("tab", presses=5, interval=.01)
+pyautogui.press("space", interval=.01) # Accompanying Names
+pyautogui.press("tab", presses=3, interval=.01)
+pyautogui.press("space", interval=.01) # Notes
+time.sleep(1)
+pyautogui.press("tab", interval=.01)
+pyautogui.press("space", interval=.01)
+pyautogui.press("tab", presses=8, interval=.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(12.5)
+# VKHL Guests INH (Full): Print
+pyautogui.click(600, 84, interval=0.01)
+time.sleep(.5)
+page_print_copy2(1)
