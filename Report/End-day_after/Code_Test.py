f2f78e913878e62560eaa837a895d9084b65beab
Thaweesak Jantasan <em.94xd@gmail.com>
2026-07-01 02:42:09 +0700

    ADD VKHL Guests INH (Comp), VKHL Guests INH (Full), Package Forecast. FIX space delay

--- Report/End-day_after/Code_Test.py
+++ Report/End-day_after/Code_Test.py
@@ -5,11 +5,21 @@ import os
 import calendar
 import subprocess
 
-Endday_after_folder = os.environ.get("USERPROFILE").__add__(r"\Documents\Runit\Report\End-day_after")
-
 site_OPERA = "https://mtca2.oraclehospitality.ap-singapore-1.ocs.oraclecloud.com/MINOR/operacloud/faces/opera-cloud-index/OperaCloud"
 
 Room_Type = "1H2XK,1H2XT,1H3XK,1H4XK,1H4XT,2U1XKT,2U2XKT,2U3XKT,1V1XK,1V2XK,3U1CKT,2V1C2K,1H1VK,1U1VK,1U2VK,2U1VKT,2U2VKT,2U3VKT,3U2VKT,1H2VK,2U4XKT,3U1C2K"
+
+Room_Class = "HRA,HRB,HRC,HRD,HSA,HSB,HSC,HSD,HSE,HVA,HVB,HVC"
+
+def report_path():
+    yesterday = date.today() - timedelta(days=1)
+    ytd_date = yesterday.strftime("%#d")
+    ytd_month_number = yesterday.strftime("%#m")
+    ytd_month_short = yesterday.strftime("%b")
+    ytd_month_full = yesterday.strftime("%B")
+    ytd_year_full = yesterday.strftime("%Y")
+
+    return fr"\\LMPC202507256L\Keeper\Daily's Report\Report {ytd_year_full}\{ytd_month_number} - {ytd_month_short} {ytd_year_full}\{ytd_date} {ytd_month_full}"
 
 def tab_reserve(times):
     for _ in range(times):
@@ -41,13 +51,24 @@ def date_end():
     month_end = today.replace(day=last_day)
     return month_end.strftime("%d%m")
 
-# Clear report store
-os.startfile(Endday_after_folder)
-time.sleep(1)
-pyautogui.press("tab", presses=13, interval=.01)
-pyautogui.hotkey("ctrl", "a", interval=.01)
-pyautogui.press("delete", interval=.01)
-pyautogui.hotkey("ctrl", "w", interval=.01)
+def report_print(report_name):
+
+    Endday_after_folder = report_path()
+
+    folder_report = os.path.join(Endday_after_folder, report_name).__add__(".PDF")
+
+    print_url_add = "file:" + folder_report.replace("\\", "/")
+
+    subprocess.run(["cmd", "/c", "start", "msedge", print_url_add])
+
+def file_remove(path):
+    for _ in os.listdir(path):
+        to_file = os.path.join(path, _)
+        if os.path.isfile(to_file):
+            os.remove(to_file)
+
+# Delete files
+file_remove(report_path())
 
 # Open Opera
 subprocess.run(["cmd", "/c", "start", "msedge", site_OPERA])
@@ -68,33 +89,30 @@ pyautogui.press("enter", interval=0.01)
 time.sleep(4.5)
 pyautogui.press("tab", interval=0.01)
 
-# VKHL Arrivals
-pyautogui.write("Arrivals: Detailed FO", interval=.01)
-pyautogui.press("enter", interval=.01)
+# Package Forecast
+pyautogui.write("Package Forecast - Detailed", interval=.01)
+pyautogui.press("enter", interval=0.01)
 time.sleep(3)
-pyautogui.press("tab", presses=9, interval=.01)
-pyautogui.press("down", presses=2, interval=.01)
+pyautogui.press("tab", presses=9, interval=0.01)
+pyautogui.press("down", presses=2, interval=0.01)
 time.sleep(1.5)
 pyautogui.press("right", interval=.01)
-pyautogui.press("tab", presses=3, interval=.01)
-pyautogui.press("enter", interval=.01)
+pyautogui.press("tab", presses=3, interval=0.01)
+pyautogui.press("enter", interval=0.01)
 time.sleep(3.5)
-# VKHL Arrivals: Config
-pyautogui.hotkey("ctrl", "a", interval=.01)
+# Package Forecast: Config
+pyautogui.press("tab", presses=2, interval=0.01)
 pyautogui.write(format1_today(), interval=.01)
-pyautogui.press("tab", presses=2, interval=.01)
+pyautogui.press("tab", interval=0.01)
 time.sleep(1)
 pyautogui.write(format1_today(), interval=.01)
-pyautogui.press("tab", presses=4, interval=.01)
+pyautogui.press("tab", presses=3, interval=0.01)
 time.sleep(.75)
-pyautogui.write(Room_Type, interval=.01)
-pyautogui.press("tab", presses=38, interval=.01)
-pyautogui.press("delete", interval=.01)
-time.sleep(.5)
-pyautogui.press("tab", presses=11, interval=.01)
+pyautogui.write("BFB01E,BFB01I,BFB02E,BFB02I,BFB03I,BFB04I", interval=.01)
+pyautogui.press("tab", presses=13, interval=0.01)
 pyautogui.press("enter", interval=0.01)
-time.sleep(10)
-# VKHL Arrivals: Save
+time.sleep(8.5)
+# Package Forecast: Save
 pyautogui.click(600, 84, interval=0.01)
 time.sleep(.5)
 pyautogui.hotkey("ctrl", "s", interval=.01)
@@ -103,24 +121,20 @@ pyautogui.hotkey("ctrl", "f", interval=.01)
 tab_reserve(2)
 time.sleep(0.1)
 pyautogui.press("enter", interval=0.01)
-pyautogui.write(Endday_after_folder)
+pyautogui.write(report_path())
 pyautogui.press("enter", interval=0.01)
-time.sleep(1)
-pyautogui.press("enter", interval=0.01)
-pyautogui.write(Endday_after_folder)
-pyautogui.press("enter", interval=0.01)
-time.sleep(1)
+time.sleep(.5)
 pyautogui.press("tab", presses=6, interval=.01)
 
-VKHL_Arrivals = "4. VKHL Arrivals"
+Package_Forecast = "19. Package Forecast"
 
-pyautogui.write(VKHL_Arrivals, interval=.01)
-# pyautogui.press("tab", presses=3, interval=.01)
-# pyautogui.press("enter", interval=0.01)
-# time.sleep(0.5)
-# pyautogui.hotkey("ctrl", "w")
-# time.sleep(0.5)
-# tab_reserve(3)
-# pyautogui.press("enter", interval=0.01)
-# time.sleep(5)
-# pyautogui.press("tab", interval=0.01)
+pyautogui.write(Package_Forecast, interval=.01)
+pyautogui.press("tab", presses=3, interval=.01)
+pyautogui.press("enter", interval=0.01)
+time.sleep(0.5)
+pyautogui.hotkey("ctrl", "w")
+time.sleep(0.5)
+tab_reserve(3)
+pyautogui.press("enter", interval=0.01)
+time.sleep(5)
+pyautogui.press("tab", interval=0.01)
