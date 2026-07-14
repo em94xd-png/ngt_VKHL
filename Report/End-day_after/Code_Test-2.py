1be28411a9a842f0495d4e7c8a04ea423f4f2035
Thaweesak Jantasan <em.94xd@gmail.com>
2026-07-14 22:25:42 +0700

    Clear

--- Report/End-day_after/Code_Test-2.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-import calendar
-import os
-
-
-Endday_after_folder = os.environ.get("USERPROFILE").__add__(r"\Documents\Runit\Report\End-day_after")
-
-print(Endday_after_folder)
