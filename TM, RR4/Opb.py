3531aef2ae9e28427c72e49456c3a2ee42ae4ea5
Thaweesak Jantasan <em.94xd@gmail.com>
2026-07-14 22:22:24 +0700

    TEST

--- TM, RR4/Opb.py
+++ TM, RR4/Opb.py
@@ -10,10 +10,13 @@ excel_file = "get_data.xlsx"
 ws1 = "Sheet1"
 ws2 = "Sheet2"
 
-tree = xml.etree.ElementTree.parse("immigration_report_140258562.XML")
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+xml_file = "immigration_report_140258562.XML"
+tree = xml.etree.ElementTree.parse(os.path.join(current_path, xml_file))
 root = tree.getroot()
 
-wb = load_workbook(excel_file)
+wb = load_workbook(os.path.join(current_path, excel_file))
 
 ws1 = wb[ws1]
 ws2 = wb[ws2]
@@ -23,14 +26,14 @@ for _ in range(ws1.max_row, 1, -1):
 for _ in range(ws2.max_row, 3, -1):
      ws2.delete_rows(_, amount=1)
 
-with open("tm_ct.json", "r", encoding="utf-8") as file:
-     tm_ct_json = json.load(file)
-with open("rr_nt.json", "r", encoding="utf-8") as file:
+with open((os.path.join(current_path, "tm_nt.json")), "r", encoding="utf-8") as file:
+     tm_nt_json = json.load(file)
+with open((os.path.join(current_path, "rr_nt.json")), "r", encoding="utf-8") as file:
      rr_nt_json = json.load(file)
-with open("rr_ct.json", "r", encoding="utf-8") as file:
+with open((os.path.join(current_path, "rr_ct.json")), "r", encoding="utf-8") as file:
      rr_ct_json = json.load(file)
 
-def sw_date_format(ad, be):
+def sw_date_format(ad, be, sc):
      if ad and ad.strip() != "":
           try:
                ad = datetime.strptime(ad.strip(), "%m/%d/%Y")
@@ -44,6 +47,12 @@ def sw_date_format(ad, be):
                return be.strftime(f"%d/%m/{cvt_be}")
           except ValueError:
                return be
+     if sc and str(sc).strip() != "":
+          try:
+               sc = datetime.strptime(sc, "%d-%m-%Y")
+               return sc.strftime("%d/%m/%Y")
+          except ValueError:
+               return sc
 
 for _ in root.findall(".//G_IMMIGRATION"):
         fn = _.find("FIRST_NAME").text
@@ -52,10 +61,10 @@ for _ in root.findall(".//G_IMMIGRATION"):
         pn = _.find("PASSPORT").text
         nt = _.find("NATIONALITY").text
         bd = _.find("DATE_OF_BIRTH").text
-        bd_ad = sw_date_format(bd, None)
+        bd_ad = sw_date_format(bd, None, None)
         dep = _.find("DEPARTURE_DATE").text
-        dep_ad = sw_date_format(dep, None)
-        dep_be = sw_date_format(None, dep)
+        dep_ad = sw_date_format(dep, None, None)
+        dep_be = sw_date_format(None, dep, None)
         rn = _.find("ROOM").text
         tm_data = [fn, None, ln, gd, pn, nt, bd_ad, dep_ad]
         rr_data = [None, None, None, rn, None, None, None, None, gd, fn, None, ln, nt, None, pn, None, None, None, None, nt, nt, None, None, dep_be, None, None]
@@ -67,32 +76,41 @@ ws3 = wb["Sheet3"]
 
 ws3_fn_ln_pn = {}
 ws3_fn_ln_ct = {}
+ws3_fn_ln_bd = {}
 for _ in ws3.iter_rows(values_only=True, min_row=2):
     ws3_fn = _[1]
     ws3_ln = _[0]    
     ws3_pn = _[4]     
     ws3_ct = _[3]
+    ws3_bd = _[2]
     if ws3_fn and ws3_ln and ws3_pn != "":
-          ws3_key = f"{str(ws3_fn).strip()}_{str(ws3_ln).strip()}"
+          ws3_key = f"{str(ws3_fn).strip().lower()}_{str(ws3_ln).strip().lower()}"
           ws3_fn_ln_pn[ws3_key] = str(ws3_pn).strip()
           ws3_fn_ln_ct[ws3_key] = str(ws3_ct).strip()
+          ws3_fn_ln_bd[ws3_key] = str(ws3_bd).strip()
 
 for _ in range(ws1.max_row, 1, -1):
-    ws1_fn = ws1.cell(row=_, column=1).value
-    ws1_ln = ws1.cell(row=_, column=3).value
-    ws1_pn = ws1.cell(row=_, column=5).value
-    ws1_ct = ws1.cell(row=_, column=6).value
-    if ws1_fn and ws1_ln != "":
-          ws1_key = f"{str(ws1_fn).strip()}_{str(ws1_ln).strip()}"
+     ws1_fn = ws1.cell(row=_, column=1).value
+     ws1_ln = ws1.cell(row=_, column=3).value
+     ws1_pn = ws1.cell(row=_, column=5).value
+     ws1_nt = ws1.cell(row=_, column=6).value
+     ws1_bd = ws1.cell(row=_, column=7).value
+     if ws1_fn and ws1_ln != "":
+          ws1_key = f"{str(ws1_fn).strip().lower()}_{str(ws1_ln).strip().lower()}"
           if ws1_key in ws3_fn_ln_pn:
                for_empty_passport = ws3_fn_ln_pn[ws1_key]
                if ws1_pn is None or str(ws1_pn).strip() == "":
                     ws1.cell(row=_, column=5, value=for_empty_passport)
-    if ws1_ct == "Thailand":
-          ws1_key = f"{str(ws1_fn).strip()}_{str(ws1_ln).strip()}"
+     if ws1_nt == "Thailand":
+          ws1_key = f"{str(ws1_fn).strip().lower()}_{str(ws1_ln).strip().lower()}"
           if ws1_key in ws3_fn_ln_ct:
                for_THA_passport = ws3_fn_ln_ct[ws1_key]  
                ws1.cell(row=_, column=6, value=for_THA_passport)
+     if ws1_bd is None or str(ws1_bd).strip() == "":
+          ws1_key = f"{str(ws1_fn).strip().lower()}_{str(ws1_ln).strip().lower()}"
+          if ws1_key in ws3_fn_ln_bd:
+               for_empty_birthday = ws3_fn_ln_bd[ws1_key]
+               ws1.cell(row=_, column=7, value=sw_date_format(None, None, for_empty_birthday))
 
 for _ in range(ws2.max_row, 3, -1):
      ws2_fn = ws2.cell(row=_, column=10).value
@@ -100,34 +118,37 @@ for _ in range(ws2.max_row, 3, -1):
      ws2_pn = ws2.cell(row=_, column=15).value
      ws2_ct = ws2.cell(row=_, column=13).value
      if ws2_fn and ws2_ln != "":
-          ws2_key = f"{str(ws2_fn).strip()}_{str(ws2_ln).strip()}"
+          ws2_key = f"{str(ws2_fn).strip().lower()}_{str(ws2_ln).strip().lower()}"
           if ws2_key in ws3_fn_ln_pn:
                for_empty_passport = ws3_fn_ln_pn[ws2_key]
                if ws2_pn is None or str(ws2_pn).strip() == "":
                     ws2.cell(row=_, column=15, value=for_empty_passport)
      if ws2_ct == "Thailand":
-          ws2_key = f"{str(ws2_fn).strip()}_{str(ws2_ln).strip()}"
+          ws2_key = f"{str(ws2_fn).strip().lower()}_{str(ws2_ln).strip().lower()}"
           if ws2_key in ws3_fn_ln_ct:
                for_THA_passport = ws3_fn_ln_ct[ws2_key]
                ws2.cell(row=_, column=13, value=for_THA_passport)
+          if ws2_key in ws3_fn_ln_ct:
+               for_THA_passport = ws3_fn_ln_ct[ws2_key]
+               ws2.cell(row=_, column=20, value=for_THA_passport)
 
 for _ in range(ws1.max_row, 1, -1):
-     ct = ws1.cell(row=_, column=6).value
-     if ct in tm_ct_json:
-          ct_match = tm_ct_json[ct]
-          ws1.cell(row=_, column=6, value=ct_match)
+     nt = ws1.cell(row=_, column=6).value
+     if nt in tm_nt_json:
+          nt_match = tm_nt_json[nt]
+          ws1.cell(row=_, column=6, value=nt_match)
 
 for _ in range(ws2.max_row, 3, -1):
      nt = ws2.cell(row=_, column=13).value
-     ct = ws2.cell(row=_, column=21).value
-     if str(nt).strip() or str(ct).strip() != "":
+     ct = ws2.cell(row=_, column=20).value
+     if nt or ct != "":
           pure_nt = str(nt).strip().lower()
           pure_ct = str(ct).strip().lower()
           for nt, cd in rr_nt_json.items():
-               if pure_nt in str(nt).lower():
+               if pure_nt[:5] in str(nt).strip().lower():
                     ws2.cell(row=_, column=13, value=cd)
           for ct, cd in rr_ct_json.items():
-               if pure_ct in str(ct).lower():
+               if pure_ct[:5] in str(ct).strip().lower():
                     ws2.cell(row=_, column=21, value=cd)
 
 for _ in range(ws1.max_row, 1, -1):
@@ -163,8 +184,23 @@ for _ in range(ws2.max_row, 3, -1):
 
 red_color = PatternFill(start_color="FFC7CE", end_color="FFC7CE", fill_type="solid")
 
+for _ in range(ws1.max_row, 1, -1):
+     gd = ws1.cell(row=_, column=4).value
+     nt = ws1.cell(row=_, column=6).value
+     bd = ws1.cell(row=_, column=7).value
+     if gd is None or str(gd).strip() == "U" or str(gd).strip() == "":
+          ws1.cell(row=_, column=4).fill = red_color
+     if nt is None or str(nt).strip() == "":
+          ws1.cell(row=_, column=6).fill = red_color
+     if len(str(nt).strip()) > 3:
+          ws1.cell(row=_, column=6).fill = red_color
+     if bd is None or str(bd).strip() == "":
+          ws1.cell(row=_, column=7).fill = red_color
+
 for _ in range(ws2.max_row, 3, -1):
      gd = ws2.cell(row=_, column=9).value
+     nt = ws2.cell(row=_, column=13).value
+     ct = ws2.cell(row=_, column=21).value
      if str(gd).strip() != "":
           if gd == "F":
                ws2.cell(row=_, column=9, value="Ms.")
@@ -172,22 +208,12 @@ for _ in range(ws2.max_row, 3, -1):
                ws2.cell(row=_, column=9, value="Mr.")
           else:
                ws2.cell(row=_, column=9).fill = red_color
-
-for _ in range(ws1.max_row, 1, -1):
-     gd = ws1.cell(row=_, column=4).value
-     nt = ws1.cell(row=_, column=6).value
-     bd = ws1.cell(row=_, column=7).value
-     if str(gd).strip() is None or str(gd).strip() == "U" or str(gd).strip() == "":
-          ws1.cell(row=_, column=4).fill = red_color
-     if nt is None or str(nt).strip() == "":
-          ws1.cell(row=_, column=6).fill = red_color
-     elif not bd is not None or str(bd).strip() == "":
-          ws1.cell(row=_, column=7).fill = red_color
-
-for _ in range(ws2.max_row, 3, -1):
-     nt = ws2.cell(row=_, column=13).value
-     if not nt or str(nt).strip() == "":
+     if (str(nt).strip() or str(ct).strip()) is None or (str(nt).strip() or str(ct).strip()) == "":
           ws2.cell(row=_, column=13).fill = red_color
+          ws2.cell(row=_, column=21).fill = red_color
+     if str(nt).strip().isalpha() or str(ct).strip().isalpha():
+          ws2.cell(row=_, column=13).fill = red_color
+          ws2.cell(row=_, column=21).fill = red_color
 
 run_num = 1
 
@@ -198,7 +224,7 @@ for _ in range(4, ws2.max_row + 1):
 for _ in range(ws2.max_row, 3, -1):
      ytd = date.today() - timedelta(days=1)
      full_ytd = ytd.strftime("%m/%d/%Y")
-     ws2.cell(row=_, column=2, value=sw_date_format(None, full_ytd))
+     ws2.cell(row=_, column=2, value=sw_date_format(None, full_ytd, None))
      ws2.cell(row=_, column=3, value="15.00")
      ws2.cell(row=_, column=17, value="Phang-nga")
      ws2.cell(row=_, column=18, value="99")
@@ -210,29 +236,48 @@ for _ in range(ws2.max_row, 3, -1):
 
 ytd = date.today() - timedelta(days=1)
 full_ytd = ytd.strftime("%m/%d/%Y")
-ws2.cell(row=2, column=14, value=sw_date_format(None, full_ytd))
+ws2.cell(row=2, column=14, value=sw_date_format(None, full_ytd, None))
 
 ytd = date.today() - timedelta(days=1)
 ytd_y = ytd.strftime("%Y")
+ytd_y_be = ytd.year + 543
 ytd_m = ytd.strftime("%B")
 tm_date = ytd.strftime("%d.%m.%Y")
+rr_date = ytd.strftime(f"%d%m{ytd_y_be}")
 
-tm_path = fr"\\LMPC202507256L\Keeper\TM\{ytd_y}\{ytd_m}"
-os.makedirs(tm_path, exist_ok=True)
+# tm_path = fr"\\LMPC202507256L\Keeper\TM\{ytd_y}\{ytd_m}"
+# os.makedirs(tm_path, exist_ok=True)
 
 tm_excel = f"{tm_date}.xlsx"
-path_tm_excel = os.path.join(tm_path, tm_excel)
+rr_excel = f"{rr_date}.xlsx"
+# path_tm_excel = os.path.join(tm_path, tm_excel)
 
-wb.save(path_tm_excel)
+# wb.save(path_tm_excel)
+# wb.save(os.path.join(current_path, tm_excel))
+wb.save(os.path.join(current_path, rr_excel))
 
-tm_wb = openpyxl.load_workbook(path_tm_excel)
-tm_ws = "Sheet1"
+# tm_wb = openpyxl.load_workbook(path_tm_excel)
+# tm_wb = openpyxl.load_workbook(os.path.join(current_path, tm_excel))
+# tm_ws = "Sheet1"
 
-for _ in tm_wb.sheetnames:
-     if _ != tm_ws:
-          del tm_wb[_]
+# for _ in tm_wb.sheetnames:
+#      if _ != tm_ws:
+#           del tm_wb[_]
 
-tm_wb.save(path_tm_excel)
+# # tm_wb.save(path_tm_excel)
+# tm_wb.save(os.path.join(current_path, tm_excel))
+
+rr_wb = openpyxl.load_workbook(os.path.join(current_path, rr_excel))
+rr_ws = "Sheet2"
+
+for _ in rr_wb.sheetnames:
+     if _ != rr_ws:
+          del rr_wb[_]
+
+rr_ws = rr_wb[rr_ws]
+rr_ws.title = "Sheet1"
+# tm_wb.save(path_tm_excel)
+rr_wb.save(os.path.join(current_path, rr_excel))
 
 # for _ in wb.worksheets:
 #      _.views.sheetView[0].tabSelected = False
