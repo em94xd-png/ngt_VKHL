cbb63701a3b50db70cff601394de5377f6d1cf81
Thaweesak Jantasan <em.94xd@gmail.com>
2026-07-23 10:31:32 +0700

    UPDATE

--- Main_Tkinter.py
+++ Main_Tkinter.py
@@ -1,9 +1,5 @@
-import customtkinter
+import customtkinter, subprocess, threading, os, json
 from tkinter import *
-import subprocess
-import threading
-import os
-import json
 
 window_main = customtkinter.CTk()
 window_main.title("ngt")
@@ -15,94 +11,74 @@ window_main.resizable(False, False)
 switch_mode = StringVar()
 
 def mode_show():
-    mode_get = switch_mode.get()
-
-    if mode_get == "on":
+    get_mode = switch_mode.get()
+    if get_mode == "on":
         customtkinter.set_appearance_mode("dark")
-    
     else:
         customtkinter.set_appearance_mode("light")
 
-drop_1_store = StringVar()
-drop_1_path = {}
+drop_1_select = StringVar()
+drop_1_link = {}
 
-def theme_path():
+def link_theme():
     path_1 = os.path.dirname(os.path.abspath(__file__))
     path_2 = os.path.join(path_1, "themes")
-    
     for _ in os.listdir(path_2):
         if _.endswith(".json"):
-            theme_key = os.path.splitext(_)[0]
-            theme_key_path = os.path.join(path_2, _)
-            drop_1_path[theme_key] = theme_key_path
+            key_theme = os.path.splitext(_)[0]
+            key_theme_link = os.path.join(path_2, _)
+            drop_1_link[key_theme] = key_theme_link
 
-theme_path()
-drop_1_items = list(drop_1_path.keys())
+link_theme()
+drop_1_list = list(drop_1_link.keys())
 
-def theme_use(_=None):
-    drop_1_get = drop_1_store.get()
-    
-    if drop_1_get in drop_1_path:
-        to_json = drop_1_path[drop_1_get]
-
+def use_theme(_=None):
+    drop_1_get = drop_1_select.get()
+    if drop_1_get in drop_1_link:
+        to_json = drop_1_link[drop_1_get]
         with open(to_json, "r", encoding="utf-8") as file:
-            from_json = json.load(file)
-
-        drop_1_json = from_json.get("CTkButton", {})
-        fg_color = drop_1_json.get("fg_color")
-        hover_color = drop_1_json.get("hover_color")
-
+            json_file = json.load(file)
+        to_CTkButton = json_file.get("CTkButton", {})
+        fg_color = to_CTkButton.get("fg_color")
+        hover_color = to_CTkButton.get("hover_color")
         if fg_color and hover_color:
             for _ in [button_1, button_2, button_3]:
                 _.configure(fg_color=fg_color, hover_color=hover_color)
 
-running_endday_before = None
-running_endday_after = None
-running_FB_print = None
+link_file_process = {"endday_before": None, "endday_after": None, "FB_print": None}
 
-def button_toggle(state):
-    button_1.configure(state=state)
-    button_2.configure(state=state)
-    button_3.configure(state=state)
+def block_button(state):
+    for _ in [button_1, button_2, button_3]:
+        _.configure(state=state)
 
-def monitor_process(process):
-    process.wait()
-    window_main.after(0, lambda: button_toggle("normal"))
+def monitor_process(_):
+    _.wait()
+    window_main.after(0, lambda: block_button("normal"))
 
-def script_run(script_path, process_var):
-    global running_endday_before, running_endday_after, running_FB_print
-
-    process_current = globals()[process_var]
-
-    if process_current and process_current.poll() is None:
+def run_script(path, key):
+    global link_file_process
+    if link_file_process[key] and link_file_process[key].poll() is None:
         return
-    
-    button_toggle("disabled")
+    block_button("disabled")
+    run_file = subprocess.Popen(["python", path])
+    link_file_process[key] = run_file
+    threading.Thread(target=monitor_process, args=(run_file,), daemon=True).start()
 
-    file_run = subprocess.Popen(["python", script_path])
-    globals()[process_var] = file_run
+def endday_before():
+    run_script(r"report\end-day_before\main_end-day_before.py", "endday_before")
 
-    threading.Thread(target=monitor_process, args=(file_run,), daemon=True).start()
-
-def report_endday_before():
-    script_run(r"report\end-day_before\main_end-day_before.py", "running_endday_before")
-
-def report_endday_after():
-    script_run(r"report\end-day_after\main_end-day_after.py", "running_endday_after")
+def endday_after():
+    run_script(r"report\end-day_after\main_end-day_after.py", "endday_after")
 
 def FB_print():
-    script_run(r"fb print\main_FB_print.py", "running_FB_print")
+    run_script(r"fb print\main_FB_print.py", "FB_print")
 
 def stop_all():
-    global running_endday_before, running_endday_after, running_FB_print
-    
-    running_active = [running_endday_before, running_endday_after, running_FB_print]
-
-    for _ in running_active:
+    global link_file_process
+    for _ in link_file_process.values():
         if _ and _.poll() is None:
             _.terminate()
-
-    button_toggle("normal")
+    block_button("normal")
 
 frame_1 = customtkinter.CTkFrame(master=window_main)
 frame_1.pack(padx=2.5, pady=2.5, fill="both", expand=True)
@@ -113,10 +89,10 @@ switch.pack(anchor=E)
 label_1 = customtkinter.CTkLabel(master=frame_1, text="OI", font=("Sans-serif", 20))
 label_1.pack(pady=(10, 25))
 
-button_1 = customtkinter.CTkButton(master=frame_1, text="Before", command=report_endday_before)
+button_1 = customtkinter.CTkButton(master=frame_1, text="Before", command=endday_before)
 button_1.pack(pady=0)
 
-button_2 = customtkinter.CTkButton(master=frame_1, text="After", command=report_endday_after)
+button_2 = customtkinter.CTkButton(master=frame_1, text="After", command=endday_after)
 button_2.pack(pady=(7.5, 0))
 
 button_3 = customtkinter.CTkButton(master=frame_1, text="FB Print", command=FB_print)
@@ -125,7 +101,7 @@ button_3.pack(pady=(7.5, 0))
 button_stop = customtkinter.CTkButton(master=frame_1, text="Stop", fg_color=["#BEC7C9", "#495054"], hover_color="#E05454", command=stop_all)
 button_stop.pack(padx=5, pady=(55, 5))
 
-drop_1 = customtkinter.CTkComboBox(master=frame_1, variable=drop_1_store, values=drop_1_items, command=theme_use)
+drop_1 = customtkinter.CTkComboBox(master=frame_1, variable=drop_1_select, values=drop_1_list, command=use_theme)
 drop_1.pack()
 
 window_main.mainloop()
