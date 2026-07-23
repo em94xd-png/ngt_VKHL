d6c3abc012021173e178a42439d551362b6f4200
Thaweesak Jantasan <em.94xd@gmail.com>
2026-07-24 01:02:17 +0700

    UPDATE

--- TM, RR4/get_data.py
+++ TM, RR4/get_data.py
@@ -17,33 +17,32 @@ def step_copy(times):
     pyautogui.hotkey("ctrl", "c")
     return pyperclip.paste().strip()
 
-def get_data():
-    while True:
-        main_title = pygetwindow.getWindowsWithTitle(main_panel)[0]
-        if not main_title.isMinimized:
-            main_title.activate()
-            time.sleep(.5)
-            ln = step_copy(3)
-            fn = step_copy(4)
-            bd = step_copy(9)
-            ct = step_copy(5)
-            pn = step_copy(6)
-            wb = openpyxl.load_workbook(path_td_snf_excel)
-            ws3 = wb["Sheet3"]
-            ws3.append([ln, fn, bd, ct, pn])
-            wb.save(path_td_snf_excel)
-            while True:
-                main_title = pygetwindow.getWindowsWithTitle(main_panel)[0]
-                if not main_title:
-                    sub_title = pygetwindow.getWindowsWithTitle(sub_panel)[0]
-                    if not sub_title:
-                        break
-
 while True:
-    if os.path.exists(path_td_snf_excel):
-        get_data()
-    if not os.path.exists(path_td_snf_excel):
-        if not os.path.exists(fr"\\{script_config.snf_path}"):
-            sys.exit()
-        shutil.copy(os.path.join(script_config.snf_path, "get_data.xlsx"), path_td_snf_excel)
-        break
+    main_title = pygetwindow.getWindowsWithTitle(main_panel)
+    sniffer = main_title[0]
+    if sniffer:
+        if os.path.exists(path_td_snf_excel):
+            if not sniffer.isMinimized:
+                sniffer.activate()
+                time.sleep(.5)
+                ln = step_copy(3)
+                fn = step_copy(4)
+                bd = step_copy(9)
+                ct = step_copy(5)
+                pn = step_copy(6)
+                wb = openpyxl.load_workbook(path_td_snf_excel)
+                ws3 = wb["Sheet3"]
+                ws3.append([ln, fn, bd, ct, pn])
+                wb.save(path_td_snf_excel)
+                while True:
+                    sniffer
+                    if not sniffer:
+                        sub_title = pygetwindow.getWindowsWithTitle(sub_panel)
+                        if not sub_title:
+                            break
+        if not os.path.exists(path_td_snf_excel):
+            if not os.path.exists(f"{script_config.snf_path}"):
+                sys.exit()
+            shutil.copy(os.path.join(os.path.dirname(os.path.abspath(__file__)), "get_data.xlsx"), path_td_snf_excel)
+            if os.path.exists(path_td_snf_excel):
+                continue
